df1e27fa5d52fe8ba0951bffa6f47e09678e3e9e
Selina Wang <26selinaw@Zhongleis-MacBook-Air.local>
2020-07-25 12:05:59 -0700

    If loop file

---
+++ If.java
@@ -0,0 +1,8 @@
+public class If {
+    public static void main (String [] args) {
+        int x=0;
+        if (x>0)
+            System .out.println ("Selina");       
+        System.out.println ("Isabella");
+    }
+}
