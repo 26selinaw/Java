c393c3787d39771753950b9a287c302d06677761
Selina Wang <26selinaw@Zhongleis-MacBook-Air.local>
2020-07-26 14:12:14 -0700

    Time class

--- Time.java
+++ Time.java
@@ -1,38 +1,46 @@
 public class Time
 {
-    /** Outputs time t in the form hrs:mins:secs. 
-     * * @param t the time
-    */
-    public void display (Time t) 
-    {
-        Time T = new Time(h, m, s);
-        System.out.println(T);
+    public static void main(final String[] args) {
+        System.out.println("started!");
+        final Time t = new Time(9, 5, 11);
     }
-    // private int hrs;
-    // private int mins;
-    // private int secs;
 
-    // public Time()
-    // { /* implementation not shown */ }
-    
-    // public Time(int h, int m, int s) 
-    // { /* implementation not shown */ }
-    
-    // public void resetTime(int h, int m, int s)
-    // { /* implementation not shown*/ }
+    private int hrs;
+    private int mins;
+    private int secs;
 
-    // public void increment()
-    // { /* implementation not shown*/ }
+    public Time() {
+        /* implementation not shown */ }
+
+    public Time(final int h, final int m, final int s) { /* implementation not shown */
+        hrs = h;
+        mins = m;
+        secs = s;
+
+        System.out.println("in construcotor method!");
+    }
+
+    public void resetTime(final int h, final int m, final int s) {
+        /* implementation not shown */ }
+
+    public void increment() {
+        /* implementation not shown */ }
 
     /** @return true if this time equals t, false otherwise */
-    //public boolean equals (Time t)
-    // { /* implementation not shown */ }
+    public boolean equals(final Time t) { /* implementation not shown */
+        return true;
+    }
 
     /** @return true if this time is earlier than t, false otherwise */
-    // public boolean lessThan(Time t)
-    // { /* implementation not shown */ }
+    public boolean lessThan(final Time t)
+    { /* implementation not shown */
+        return true;
+    }
 
     /** @return a String with the time in the form hrs:mins:secs */
-    // public String toString()
-    // { /* implementation not shown */}
+    public String toString()
+    { /* implementation not shown */
+        return "hi";
+    }
+      
 }
