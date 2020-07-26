29546c9f26510c6783be39f1dd866fa987eb2493
Selina Wang <26selinaw@Zhongleis-MacBook-Air.local>
2020-07-26 12:28:43 -0700

    Time file

---
+++ Time.java
@@ -0,0 +1,38 @@
+public class Time
+{
+    /** Outputs time t in the form hrs:mins:secs. 
+     * * @param t the time
+    */
+    public void display (Time t) 
+    {
+        Time T = new Time(h, m, s);
+        System.out.println(T);
+    }
+    // private int hrs;
+    // private int mins;
+    // private int secs;
+
+    // public Time()
+    // { /* implementation not shown */ }
+    
+    // public Time(int h, int m, int s) 
+    // { /* implementation not shown */ }
+    
+    // public void resetTime(int h, int m, int s)
+    // { /* implementation not shown*/ }
+
+    // public void increment()
+    // { /* implementation not shown*/ }
+
+    /** @return true if this time equals t, false otherwise */
+    //public boolean equals (Time t)
+    // { /* implementation not shown */ }
+
+    /** @return true if this time is earlier than t, false otherwise */
+    // public boolean lessThan(Time t)
+    // { /* implementation not shown */ }
+
+    /** @return a String with the time in the form hrs:mins:secs */
+    // public String toString()
+    // { /* implementation not shown */}
+}
