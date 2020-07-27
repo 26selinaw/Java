f42916ec7a1229748e3413636f001325fae9e16f
Selina Wang <26selinaw@Zhongleis-MacBook-Air.local>
2020-07-27 10:49:07 -0700

    Test file

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static String doSomething(String s) 
+    {
+        final String BLANK = " "; //BLANK contains a single space String str = ""; //empty string
+        String temp;
+        for (int i = 0; i < s.length(); i++)
+        {
+            temp = s.substring(i, i + 1); 
+            if (!(temp.equals(BLANK)))
+                str += temp;
+        }
+        return str;
+    }
+}
