1502d9794542d59b50c66694025301f01f90cc6b
Selina Wang <26selinaw@Zhongleis-MacBook-Air.local>
2020-07-27 10:37:14 -0700

    Tester file

--- Tester.java
+++ Tester.java
@@ -1,3 +1,15 @@
+public class TempTest
+{   
+    public static void main(String[] args) 
+    {
+    System.out.println("Enter temperature scale: ");
+    String tempScale = IO.readString(); 
+    //read user input System.out.println("Enter number of degrees: ");
+    double tempDegrees = IO.readDouble(); 
+    //read user input 
+    /* code to construct a valid temperature from user input */
+    } 
+}
 public class Tester 
 {
     public void someMethod(int a, int b) 
