31a4fdafa1b9f0be62495d0d8305b137b8f85ed7
Selina Wang <26selinaw@Zhongleis-MacBook-Air.local>
2020-07-28 16:32:30 -0700

    Animation file

---
+++ Animation.java
@@ -0,0 +1,27 @@
+/*import static java.lang.Thread.sleep; */
+
+public class Animation {
+    public static void main(String[] args) {
+        
+        
+        
+        
+        /*c har x = '*';
+        int y = 0;
+        char z = ' ';
+        int n = 0;
+        while (y < 10) {
+            n += 1;
+            int inner = 0;
+            while (inner < n) {
+                System.out.print(z);
+                inner += 1;
+                //sleep(500);
+            }
+            System.out.println(x);
+            // System.out.flush();
+            y += 1;
+        } */
+        
+    }
+}
