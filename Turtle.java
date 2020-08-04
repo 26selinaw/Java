08d08c5516856c9a5ecf1573eeef711540ac3c56
Selina Wang <26selinaw@Zhongleis-MacBook-Air.local>
2020-08-04 14:38:04 -0700

    Test file that is codenamed Turtle

---
+++ Turtle.java
@@ -0,0 +1,12 @@
+public class Turtle {
+    public static void main (String [] args) {
+        int newNum = 0, temp; 
+        int num = k;
+        while (num > 10) {
+            temp = num % 10;
+            num /= 10;
+            newNum = newNum * 10 + temp;
+        } 
+        System.out.print(newNum);
+    }
+} 
