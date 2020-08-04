8704594e04fc20b8c430ed0517b6546aed76f131
Selina Wang <26selinaw@Zhongleis-MacBook-Air.local>
2020-08-04 15:12:20 -0700

    Reverse file

---
+++ Reverse.java
@@ -0,0 +1,9 @@
+public class Reverse {
+    public static void main (String [] args) {
+        int reverse(int n)
+        {
+            int rem, revNum = 0;
+            return revNum;
+        }
+    }
+}
