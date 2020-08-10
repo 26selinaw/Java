77c80d38da69adc26745fc38b2b6d5be9b28ff37
Selina Wang <26selinaw@Zhongleis-MacBook-Air.local>
2020-08-10 13:57:10 -0700

    move to chapter 1 question 1

---
+++ chapter1-q1/Main.java
@@ -0,0 +1,11 @@
+package com.selina;
+
+public class Main {
+    public static void main (final String[] args) {
+        System.out.println ("start time");
+
+        Time time = new Time(17, 35, 38);
+        
+        System.out.println(time.toString());
+    }
+}
