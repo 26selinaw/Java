b68ee39b963ecb8f3e99b7af057d9c0b048f18d3
Selina Wang <26selinaw@leozwangs-MBP.attlocal.net>
2021-03-27 10:15:38 -0700

    Python - find equation given 2 points

--- equation.py
+++ equation.py
@@ -1,7 +1,7 @@
 x1 = 1
-x2 = 3 
+x2 = 9 
 y1 = 2 
 y2 = 4
 m = (y1-y2)/(x1-x2)
 b = (x1*y2-y1*x2)/(x1-x2)
-print('y=', m, 'x +', b)
+print  'point 1: (',x1,',',y1,') point 2:(',x2,',',y2,') Equation: y =',m, 'x +', b
