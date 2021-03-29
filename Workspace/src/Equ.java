21a8e6484a2d980bd177069a06a45be209024f87
Selina Wang <26selinaw@leozwangs-MBP.attlocal.net>
2021-03-29 13:56:29 -0700

    Equation - ax+by=c

--- Workspace/src/Equ.java
+++ Workspace/src/Equ.java
@@ -1,14 +1,14 @@
 public class Equ {
     public static void main(String[] args) throws Exception {
-        int a1 = 3;
-        int b1 = -4;
-        int c1 = 17;
-        int a2 = 1;
-        int b2 = 2;
-        int c2 = 1;
+        float a1 = 3;
+        float b1 = -4;
+        float c1 = 17;
+        float a2 = 1;
+        float b2 = 2;
+        float c2 = 1;
         float y = ((c1*a2)-(c2*a1))/((b1*a2)-(b2*a1));
         float x = ((c1*b2)-(c2*b1))/((a1*b2)-(a2*b1));
-        System.out.printf ("Equation 1: %dx+%dy=%d Equation 2: %dx+%dy=%d x = %f y = %f \n", a1, b1, c1, a2, b2, c2, x, y);
+        System.out.printf ("Equation 1: %fx+%fy=%f Equation 2: %fx+%fy=%f x = %f y = %f \n", a1, b1, c1, a2, b2, c2, x, y);
 
     }
 }
