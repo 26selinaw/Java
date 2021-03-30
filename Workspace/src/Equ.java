7df0414d662a26b1ca6b7e9bca2b165ab416d3e8
Selina Wang <26selinaw@leozwangs-MBP.attlocal.net>
2021-03-29 21:08:17 -0700

    Equ - ax+bx=c

--- Workspace/src/Equ.java
+++ Workspace/src/Equ.java
@@ -1,14 +1,14 @@
 public class Equ {
     public static void main(String[] args) throws Exception {
-        float a1 = 3;
-        float b1 = -4;
-        float c1 = 17;
-        float a2 = 1;
-        float b2 = 2;
-        float c2 = 1;
+        float a1 = 99681257;
+        float b1 = 5833498;
+        long c1 = 88888254389L;
+        long a2 = 83457908423L;
+        long b2 = 123894289121822L;
+        long c2 = 12389246580234L;
         float y = ((c1*a2)-(c2*a1))/((b1*a2)-(b2*a1));
         float x = ((c1*b2)-(c2*b1))/((a1*b2)-(a2*b1));
-        System.out.printf ("Equation 1: %fx+%fy=%f Equation 2: %fx+%fy=%f x = %f y = %f \n", a1, b1, c1, a2, b2, c2, x, y);
+        System.out.printf ("Equation 1: %fx+%fy=%d Equation 2: %dx+%dy=%d x = %f y = %f \n", a1, b1, c1, a2, b2, c2, x, y);
 
     }
 }
--- Workspace/src/Equ.py
+++ Workspace/src/Equ.py
@@ -4,6 +4,6 @@ c1 = 17
 a2 = 1
 b2 = 2
 c2 = 1
-float y =((c1*a2)-(c2*a1))/((b1*a2)-(b2*a1))
-float x =((c1*b2)-(c2*b1))/((a1*b2)-(a2*b1))
+y =((c1*a2)-(c2*a1))/((b1*a2)-(b2*a1))
+x =((c1*b2)-(c2*b1))/((a1*b2)-(a2*b1))
 print 'Equation 1:', a1,'x +', b1, 'y =', c1, 'Equation 2:', a2, 'x +', b2, 'y =',c2, ' x =', x, 'y =', y
