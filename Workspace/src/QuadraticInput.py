224c2d696d3210d703420b2daa7b41410695c722
Selina Wang <26selinaw@leozwangs-MacBook-Pro.local>
2021-04-07 10:17:21 -0700

    Quadratic Formula Calculator - User Input

--- Workspace/src/QuadraticInput.py
+++ Workspace/src/QuadraticInput.py
@@ -5,12 +5,11 @@ x1 = (-b - ((b*b-4*a*c) ** 0.5) )/2*a
 x2 = (-b + ((b*b-(4*a*c)) ** 0.5) )/2*a
 f1 = -x1
 f2 = -x2
-if a==1 or a==1:
-    print '( x +',f1,')( x +',f2,')'
+if a==1 or a == -1:
     print 'Equation:', a,'x^2+', b,'x=',c
     print 'x=',x1, 'and', x2
+    print '( x +',f1,')( x +',f2,')'
 if a==0:
     print 'This is not a Quadratic Equation.'
 if a < -1:
     pass
-    
