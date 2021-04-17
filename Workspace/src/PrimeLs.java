f181130b9c730a1e1302aca68ddd90654d89043b
Selina Wang <26selinaw@leozwangs-MBP.attlocal.net>
2021-04-17 16:40:36 -0700

    Prime Number 5-200

--- Workspace/src/PrimeLs.java
+++ Workspace/src/PrimeLs.java
@@ -1,9 +1,14 @@
 import java.util.*;
+import java.util.Scanner;
 public class PrimeLs {  
     public static void main(String args[]){    
-        int x=5;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the minimum: ");
+        int x = input.nextInt();   
+        System.out.print("Enter the maximum: ");
+        int y = input.nextInt();   
         List<Integer> list=new ArrayList<Integer>();
-        while (x<=200) {
+        while (x<=y) {
             for (int i = 2; i < x; i++) {
                 if (x % i == 0) {
                     break;
