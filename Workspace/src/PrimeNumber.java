b7e4ea3a6063778bab31ed9b99b02bc7d9a64a6a
Selina Wang <26selinaw@leozwangs-MBP.attlocal.net>
2021-04-17 16:00:56 -0700

    你好，这是我的Java 成勋.

--- Workspace/src/PrimeNumber.java
+++ Workspace/src/PrimeNumber.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class PrimeNumber {  
     public static void main(String[] args) {
-        int x = 739;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int x = input.nextInt();
         for (int i = 2; i < x; i++) {
             if (x % i == 0) {
                 System.out.printf ("this is not a prime number");
