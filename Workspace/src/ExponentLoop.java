adf2270e6e691ef8070b1a7c7ed06249a56b1e8c
Selina Wang <26selinaw@leozwangs-MBP.attlocal.net>
2021-06-27 18:32:03 -0700

    exponents using loops and matrix 2D arrays

--- Workspace/src/ExponentLoop.java
+++ Workspace/src/ExponentLoop.java
@@ -2,8 +2,7 @@ import java.util.*;
 import java.util.Scanner;
 public class ExponentLoop 
 {
-    public static void main(String s[])
-    {
+    public static void caculateExponent() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Base Number: ");
         int b = input.nextInt();
@@ -16,5 +15,49 @@ public class ExponentLoop
             n *= b;
         }          
         System.out.printf("Result is %d\n", n);
+
+        input.close();
+    }
+
+    public static void printArray() {
+        /*
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 7;
+        array[3] = 8;
+        array[4] = 9;
+        */
+        int[] array = new int[] {1, 2, 7, 8, 9};
+
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+            System.out.printf("array[%d]: %d\n", i, array[i]);
+        }
+
+        System.out.printf("2-D array\n");
+
+        int[][] tD = new int[][] {
+            {1, 2, 0},
+            {7, 8, 6},
+            {2, 18, 9},
+        };
+
+        int rows = tD.length;
+        int cols = tD[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                // System.out.printf("array[%d][%d]: %d ", i, j, tD[i][j]);
+                System.out.printf("%2d ", tD[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String s[]) {
+        printArray();
+
+        // caculateExponent();
     }
 }
